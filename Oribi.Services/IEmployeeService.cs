cf8832de9c8490bf95a126fc9e99d0aa5c8aaef5
Francis Ndangi <francis.ndangi@datonomy.co.za>
2021-01-26 03:14:04 +0200

    Added Persistence and Services

---
+++ Oribi.Services/IEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using Oribi.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Oribi.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee newEmployee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task DeleteAsync(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+
+    }
+}
